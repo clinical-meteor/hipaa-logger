513568e66e12ff4df29315c6078cc4e19baabd9f
Abigail Watson <awatson1978@gmail.com>
2016-01-15 17:57:39 -0800

    removed TinyTest from Gagarin test file

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hipaa-logger',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'Write HIPAA events to a logging collection.  No UI provided.',
   git: 'http://github.com/clinical-meteor/hipaa-logger',
   documentation: 'README.md'
