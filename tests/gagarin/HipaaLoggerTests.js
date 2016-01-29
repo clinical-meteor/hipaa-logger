9fad358b37ffcbe32673b6f863f0fe3f6992caea
Abigail Watson <awatson1978@gmail.com>
2016-01-29 11:29:57 -0800

    added gagarin tests

---
+++ tests/gagarin/HipaaLoggerTests.js
@@ -0,0 +1,27 @@
+describe('clinical:hipaa-logger', function () {
+  var server = meteor();
+  var client = browser(server);
+
+  it('Should exist on the client', function () {
+    return client.execute(function () {
+      expect(HipaaLogger).not.to.be.empty;
+    });
+  });
+
+  it('Should exist on the server', function () {
+    return server.execute(function () {
+      expect(HipaaLogger).not.to.be.empty;
+    });
+  });
+
+  // it('CSV.parse() parses CSV to JSON on server', function () {
+  //   return server.execute(function () {
+  //     var parsedData = CSV.parse();
+  //   });
+  // });
+  // it('CSV.parse() parses CSV to JSON on client', function () {
+  //   return client.execute(function () {
+  //     var parsedData = CSV.parse();
+  //   });
+  // });
+});
