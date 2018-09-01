c867dc3a7501932700ab0afb4c314589fc17796f
Abigail Watson <abigailwatson@Abigails-Mac-mini-379101.local>
2018-09-01 11:18:47 -0500

    simpl-schema updates; hipaa-logger support

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hipaa-logger',
-  version: '1.3.0',
+  version: '2.0.0',
   summary: 'Write HIPAA events to a logging collection.  No UI provided.',
   git: 'http://github.com/clinical-meteor/hipaa-logger',
   documentation: 'README.md'
@@ -10,6 +10,9 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
 
   api.use('meteor-platform');
+  api.use('ecmascript@0.9.0');
+
+  api.use('clinical:hl7-resource-audit-event@1.5.0');
 
   api.addFiles('lib/HipaaLog.js');
   api.addFiles('lib/HipaaLogger.js');
@@ -19,3 +22,7 @@ Package.onUse(function(api) {
   api.export('HipaaLogger');
   api.export('HipaaLog');
 });
+
+Npm.depends({
+  "simpl-schema": "1.5.3"
+})
--- server/methods.js
+++ server/methods.js
@@ -1,24 +1,59 @@
-
+import { AuditEventSchema } from 'meteor/clinical:hl7-resource-audit-event'
 
 Meteor.methods({
   logEvent: function(payload){
     check(payload, Object);
 
-    return HipaaLog.insert(payload, function(error, result){
-      if (error) {
-        console.log("error", error);        
-      }
-    });
+    var newAuditId;
+
+    if(Package['clinical:hl7-resource-audit-event']){
+      newAuditId = AuditEvents.insert(payload, function(error, result){
+        if (error) {
+          console.log("error", error);        
+        }
+      });
+    } else {
+      newAuditId = HipaaLog.insert(payload, function(error, result){
+        if (error) {
+          console.log("error", error);        
+        }
+      });  
+    }
+
+    if(process.env.DEBUG){
+      console.log("Just logged an event: ", newAuditId);
+    }
+    return newAuditId;
   },
   logHipaaEvent:function(hipaaEvent){
     check(hipaaEvent, Object);
 
-    hipaaEvent.timestamp = new Date();
-
-    var hipaaRecordId = HipaaLog.insert(hipaaEvent);
-    if(process.env.DEBUG){
-      console.log("hipaaRecordId", hipaaRecordId);
+    if(process.env.TRACE){
+      console.log("Received an event to log: ", hipaaEvent);
+      console.log('Is valid AuditEvent:  ', AuditEventSchema.validate(hipaaEvent))      
     }
-    return hipaaRecordId;
+
+    var newAuditId;
+    // is the official FHIR compliant infrastructure installed?
+    if(AuditEventSchema.validate(hipaaEvent)){
+
+      newAuditId = AuditEvents.insert(hipaaEvent, function(error, result){
+        if (error) {
+          console.log("error", error);        
+        }
+      });
+    } else {
+      hipaaEvent.timestamp = new Date();
+      newAuditId = HipaaLog.insert(hipaaEvent, function(error, result){
+        if (error) {
+          console.log("error", error);        
+        }
+      });  
+    }
+
+    if(process.env.DEBUG){
+      console.log("Just logged an event: ", newAuditId);
+    }
+    return newAuditId;
   }
 })
